3edfccb9291b4cff2c2654e5042aed2dd041f75c
Kmtengo <j12-4195-2019@student.mksu.ac.ke>
2024-04-03 21:18:32 +0300

    added stateless widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,22 @@ import 'package:flutter/material.dart';
 
 // The main function is the starting point for all our Flutter apps.
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("e-Mess"),
-        centerTitle: true,
-        backgroundColor: Colors.tealAccent,
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("e-Mess"),
+          centerTitle: true,
+          backgroundColor: Colors.tealAccent,
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
